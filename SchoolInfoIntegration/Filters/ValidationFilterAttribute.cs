77d45bc66912c5bf54052ee6e6e00448b157792f
Pushan Mitra <pushan.mitra1@ca.ey.com>
2024-03-18 13:10:12 -0700

    IOSAS/ISFS | API | Add more validation logs in validation filter

--- SchoolInfoIntegration/Filters/ValidationFilterAttribute.cs
+++ SchoolInfoIntegration/Filters/ValidationFilterAttribute.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ECC.Institute.CRM.IntegrationAPI.Filters
 {
     public class ValidationFilterAttribute : IActionFilter
     {
+        public static string ConvertModelStateErrorsToString(ModelStateDictionary modelState)
+        {
+            return string.Join(" | ", modelState.Values
+            .SelectMany(x => x.Errors)
+            .Select(x => x.ErrorMessage));
+        }
         public void OnActionExecuting(ActionExecutingContext context)
         {
             if (!context.ModelState.IsValid)
             {
-                System.Console.WriteLine("ValidationFilterAttribute: Validation error");
                 context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+                System.Console.WriteLine($"ValidationFilterAttribute: Validation error: {ConvertModelStateErrorsToString(context.ModelState)}");
             } else if (context.ActionArguments.ContainsKey("applicationName"))
             {
                 var applicationName = context.ActionArguments["applicationName"];
